b7d8cbd64ceea640d9febaf7e1eb3adc9d73236d
StefanoPezzuto <s314964@studenti.polito.it>
2024-06-29 17:08:50 +0200

    Upload PPO

---
+++ PPO/testPPO.py
@@ -0,0 +1,51 @@
+"""Test an RL agent on the OpenAI Gym Hopper environment"""
+import argparse
+
+import torch
+import gym
+import os
+import env
+from env.custom_hopper import *
+from stable_baselines3 import PPO
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', default='modelPPO', type=str, help='Model path')
+    parser.add_argument('--device', default='cpu', type=str, help='network device [cpu, cuda]')
+    parser.add_argument('--render', default=False, action='store_true', help='Render the simulator')
+    parser.add_argument('--episodes', default=100, type=int, help='Number of test episodes')
+    parser.add_argument('--test', default='target', type=str, help='Testing environment')
+
+    return parser.parse_args()
+
+args = parse_args()
+N_ENVS = os.cpu_count()
+returns = np.zeros(args.episodes)
+def main():
+    if args.test == 'source':
+        test_env = gym.make('CustomHopper-source-v0')
+    else:
+        test_env = gym.make('CustomHopper-target-v0')
+    model = PPO.load(args.model)
+
+    for episode in range(args.episodes):
+        done = False
+        test_return = 0
+        state = test_env.reset()
+        
+        while not done:
+            action, _ = model.predict(state, deterministic=True)
+            state, reward, done, info = test_env.step(action)
+            if args.render:
+                test_env.render()  
+            test_return += reward
+
+        print(f"Episode: {episode} | Return: {test_return}")
+        returns[episode] = test_return
+    
+    mean_reward = np.mean(returns)
+    print(f"Mean reward on test episodes is:{mean_reward}")
+	
+
+if __name__ == '__main__':
+	main()    
---
+++ PPO/trainPPO.py
@@ -0,0 +1,101 @@
+import gym
+import argparse
+from stable_baselines3 import PPO
+from env.custom_hopper import *
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.callbacks import StopTrainingOnMaxEpisodes, CallbackList
+import os
+import csv
+import time
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train', default='source', type=str, help='Training environment')
+    parser.add_argument('--validation', default='source', type=str, help='Validation environment')
+    parser.add_argument('--render', default=False, action='store_true', help='Render the simulator')
+    parser.add_argument('--val-episodes', default=10, type=int, help='Number of validation episodes')
+    parser.add_argument('--n-episodes', default=10750, type=int, help='Number of training episodes')
+    parser.add_argument('--device', default='cpu', type=str, help='network device [cpu, cuda]')
+
+    return parser.parse_args()
+
+args = parse_args()
+N_ENVS = os.cpu_count()
+ENV_EPS = int(np.ceil(args.n_episodes/N_ENVS))
+
+# Callback useful to evaluate training convergence  
+class CustomEvalCallback(BaseCallback):
+    def __init__(self, eval_env, n_eval_episodes=args.val_episodes, log_path=None, verbose=1):
+        super(CustomEvalCallback, self).__init__(verbose)
+        self.eval_env = eval_env
+        self.n_eval_episodes = n_eval_episodes
+        self.log_path = log_path
+        self.episode_rewards = []
+        self.episode_count = 0
+        if self.log_path is not None:
+            os.makedirs(os.path.dirname(self.log_path), exist_ok=True)
+            with open(self.log_path, mode='w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(["Episode", "Reward"])
+
+    def _on_step(self) -> bool:
+        # Check if an episode is done
+        if any(self.locals.get("dones")):
+            self.episode_count += 1
+            self._evaluate()
+        return True
+
+    def _evaluate(self):
+        episode_returns = []
+        for _ in range(self.n_eval_episodes):
+            obs = self.eval_env.reset()
+            done = False
+            episode_returns = 0.0
+            while not done:
+                action, _ = self.model.predict(obs, deterministic=True)
+                obs, reward, done, info = self.eval_env.step(action)
+                episode_returns += reward
+            episode_returns.append(episode_returns)
+
+        mean_return = np.mean(episode_returns)
+
+        if self.log_path is not None:
+            with open(self.log_path, mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([self.episode_count, mean_return])
+
+
+def main():
+    if args.train == 'source':
+        train_env = make_vec_env('CustomHopper-source-v0', n_envs=N_ENVS, vec_env_cls=DummyVecEnv)
+    else:
+        train_env = make_vec_env('CustomHopper-target-v0', n_envs=N_ENVS, vec_env_cls=DummyVecEnv)
+    
+    if args.validation == 'source':
+        val_env = gym.make('CustomHopper-source-v0')
+    else:
+        val_env = gym.make('CustomHopper-target-v0')
+
+    log_dir = 'log'
+    os.makedirs(log_dir, exist_ok=True)
+    log_path = os.path.join(log_dir, 'PPO_validation.csv')
+    stop_callback = StopTrainingOnMaxEpisodes(max_episodes=ENV_EPS, verbose=1) # Callback for stopping criteria
+    eval_callback = CustomEvalCallback(eval_env=val_env, 
+                                       n_eval_episodes=args.val_episodes, 
+                                       log_path= log_path) # Validation during training
+
+    callback_list = [stop_callback, eval_callback]
+    callback = CallbackList(callback_list)
+
+    model = PPO('MlpPolicy', env=train_env, learning_rate=0.00025, gamma=0.99, verbose=1, device=args.device)
+    start = time.time()
+    model.learn(total_timesteps=1e9, callback=callback)
+    end = time.time()
+    model.save('modelPPO')
+    total_time = end - start
+    print(f"Computational Time: {total_time}")
+
+if __name__ == '__main__':
+    main()
